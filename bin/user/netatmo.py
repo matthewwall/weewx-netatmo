4f78f1e41ea7d01cb250460e90df3c41c3be7906
Matthew Wall <matthewwall@users.noreply.github.com>
2018-07-08 15:58:01 -0400

    explicitly catch socket.timeout

--- bin/user/netatmo.py
+++ bin/user/netatmo.py
@@ -42,7 +42,7 @@ import weewx.units
 import weewx.wxformulas
 
 DRIVER_NAME = 'netatmo'
-DRIVER_VERSION = "0.10"
+DRIVER_VERSION = "0.11"
 
 INHG_PER_MBAR = 0.0295299830714
 MPH_TO_KPH = 1.60934
@@ -317,7 +317,7 @@ class CloudClient(Collector):
                     try:
                         CloudClient.get_data(self._sd, self._device_id)
                         break
-                    except (urllib2.HTTPError, urllib2.URLError), e:
+                    except (socket.error, socket.timeout, urllib2.HTTPError, urllib2.URLError), e:
                         logerr("failed attempt %s of %s to get data: %s" %
                                (tries + 1, self._max_tries, e))
                         logdbg("waiting %s seconds before retry" %
